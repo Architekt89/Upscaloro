c026464cf9e4a7478cf5c69f3639eb45c684229f
Business Laptop <businesslaptop@Businesss-MacBook-Air.local>
2025-03-17 00:16:35 +0100

    Redirect to account page

---
+++ frontend/.next/static/chunks/app/pricing/page-86f2075eaa649ce4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6907],{5474:(e,s,_)=>{Promise.resolve().then(_.bind(_,9119))}},e=>{var s=s=>e(e.s=s);e.O(0,[5714,6385,5037,9119,8441,6587,7358],()=>s(5474)),_N_E=e.O()}]);
---
+++ frontend/.next/static/chunks/app/terms/page-2070758f00e9ae2d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1366,7066,9877],{4404:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8173,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[8441,6587,7358],()=>s(4404)),_N_E=e.O()}]);
